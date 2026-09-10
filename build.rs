36d2f64c8da9c4a3ff9910711419539a08af0547
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-09-10 20:16:02 +0530

    reduce deps

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(web_sys_unstable_apis)");
+    if std::env::var("CARGO_FEATURE_UNSTABLE_APIS").is_ok() {
+        println!("cargo::rustc-cfg=web_sys_unstable_apis");
+    }
 }
